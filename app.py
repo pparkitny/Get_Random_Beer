35e4f0ba059911a798f7b195e9176a3ade9186ba
Patryk <pat.parkitny@gmail.com>
2022-04-30 23:44:31 +0200

    Add site connected with API to get random beer

--- app.py
+++ app.py
@@ -7,17 +7,26 @@ from flask import (
     url_for,
     current_app
 )
+import json
 import urllib.request
-from urllib.parse import urlparse,urljoin
+from urllib.parse import urlparse, urljoin
 from bs4 import BeautifulSoup
-import requests, validators, json,uuid,pathlib,os
+import requests, validators, json, uuid, pathlib, os
 app = Flask(__name__)
 
 
-@app.route("/",methods=("GET", "POST"), strict_slashes=False)
-def index():
-    # Actual parsing codes goes here
-    return render_template("index.html")
+@app.route('/')
+def get_beer():
+    r = requests.get('https://api.punkapi.com/v2/beers/random')
+    beerjson = r.json()
+    beer = {
+        'name': beerjson[0]['name'],
+        'abv': beerjson[0]['abv'],
+        'desc': beerjson[0]['description'],
+        'foodpair': beerjson[0]['food_pairing'][0],
+    }
+    # print(beer)
+    return render_template('index.html', beer=beer)
 
 
 if __name__ == "__main__":
